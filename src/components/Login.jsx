c28cfdac5a11a4daae34ec90ab89fd52284f5b82
michael <michaelsanchez8815@gmail.com>
2021-12-01 23:08:15 -0600

    function design for login. next step is logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,111 +1,169 @@
-import React from 'react';
-import s from '../components/Login.module.css';
+import React from "react";
+import s from "../components/Login.module.css";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            eye: undefined
-        };
-    }
-
-    handleRadioButton = e => {
-        const {name , value } = e.target;
-
-        this.setState({
-            [name]: value
-        });
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      eye: undefined,
     };
+  }
 
-    newUser = () => {
-        // const wrongForm = document.getElementById('returningUserForm');
-        // if (wrongForm) wrongForm.remove();
-        // const form = document.querySelector('form');
-        // const div = document.createElement('div');
-        // div.setAttribute('id', 'newUserForm')
-        // div.innerHTML = `
-        // test1
-        // <button class="btn btn-primary round-pill">Submit</button>        
-        // `;
-        // form.appendChild(div);
-        return (
-            <>
-                <h2 className={`header-md`}>Create an Account</h2>
-                <label>Your E-Mail Address *</label>
-                <input type="email" name="userName" id="userName" />
-                <label>Create Password *</label>
-                <div className={``}>
-                    <input className={``} type="password" name="password" id="password" />
-                    <input type="button" value={this.state.eye} onChange={this.eye}/> {/* function needed for eye flip */}
-                </div>
-                <p>
-                    Password must be 8-20 characters, including: at least one capital letter, at least one small letter, one number and one special character - ! @ # $ % ^ & * () _ + 
-                </p>
-                <label>Confirm Password *</label>
-                <div className={` `}>
-                <input className={` `} type="password" name="confirmPassword" id="confirmPassword" />
-                </div>
-                <label>First Name *</label>
-                <input type="text" name="firstName" id="firstName" />
-                <label>Last Name *</label>
-                <input type="text" name="lastName" id="lastName" />
-                <label>PostCode</label>
-                <input type="number" name="postCode" id="postCode" />
-                <button type="submit">SAVE</button>
-                <div className={s.surroundingLine}>
-                    or
-                </div>
-                {/* facebook icon */}
-                {/* cancel -> reset and go back to storeDisplay? */}
-                {/* Privacy Policy and Cookies | Terms of Sale and use // create fake places for these */}
-                {/* then create the logic */}
-            </>
-        )
-    }
+  handleRadioButton = (e) => {
+    const { name, value } = e.target;
 
-    returningUser = () => {
-        // const wrongForm = document.getElementById('newUserForm');
-        // if (wrongForm) wrongForm.remove();
-        // const form = document.querySelector('form');
-        // const div = document.createElement('div');
-        // div.setAttribute('id', 'returningUserForm')
-        // div.innerHTML = `
-        // test2
-        // <button class="btn btn-primary round-pill">Submit</button>        
-        // `;
-        // form.appendChild(div);
+    this.setState({
+      [name]: value,
+    });
+  };
 
-        return (
-            <>
-                <div>test2</div>
-                <h2 className={`header-md`}>Returning User</h2>
-            </>
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-        )
-    }
+  newUser = () => {
+    return (
+      <div className={`${s.flexContainer}`}>
+        <h2 className={`header-sm`}>Create an Account</h2>
+        <label className={s.marginAndPadding}>Your E-Mail Address *</label>
+        <input
+          className={`${s.input} ${s.marginAndPadding}`}
+          type="email"
+          name="userName"
+          id="userName"
+        />
+        <label className={s.marginAndPadding}>Create Password *</label>
+        <div className={`${s.passwordWithEye} ${s.marginAndPadding}`}>
+          <input
+            className={`${s.deleteBorder}`}
+            type="password"
+            name="password"
+            id="password"
+          />
+          <input type="button" value={this.state.eye} onChange={this.eye} />{" "}
+          {/* function needed for eye flip */}
+        </div>
+        <p className={`${s.finePrint}`}>
+          Password must be 8-20 characters, including: at least one capital
+          letter, at least one small letter, one number and one special
+          character - ! @ # $ % ^ & * ( ) _ +
+        </p>
+        <label className={s.marginAndPadding}>Confirm Password *</label>
+        <input
+          className={`${s.input} ${s.marginAndPadding}`}
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+        />
+        <label className={s.marginAndPadding}>First Name *</label>
+        <input
+          className={`${s.input} ${s.marginAndPadding}`}
+          type="text"
+          name="firstName"
+          id="firstName"
+        />
+        <label className={s.marginAndPadding}>Last Name *</label>
+        <input
+          className={`${s.input} ${s.marginAndPadding}`}
+          type="text"
+          name="lastName"
+          id="lastName"
+        />
+        <label className={s.marginAndPadding}>PostCode</label>
+        <input
+          className={`${s.input} ${s.marginAndPadding}`}
+          type="number"
+          name="postCode"
+          id="postCode"
+        />
+        <button className={`${s.input} ${s.marginAndPadding}`} type="submit">
+          SAVE
+        </button>
+        <div className={` ${s.width} ${s.surroundingLine}`}>or</div>
+        <button className={`${s.input} ${s.marginAndPadding}`} type="submit">
+          SIGN UP WITH FACEBOOK
+        </button>
+        <button
+          className={`${s.input} ${s.marginAndPadding}`}
+          onClick={this.backToStore}
+          type="reset"
+        >
+          Cancel
+        </button>
+        <div className={`${s.formFooter}`}>
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Privacy Policy and Cookies</a>
+            | 
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Terms of Sale and Use</a>
+        </div>
+        {/* then create the logic */}
+      </div>
+    );
+  };
 
-    loginType = () => this.state.userType === 'newUser' ? true : false;
+  returningUser = () => {
+    return (
+      <div className={`${s.flexContainer}`}>
+        <h2 className={`header-sm`}>Returning User</h2>
+        <label className={s.marginAndPadding}>Your E-Mail Address *</label>
+        <input
+          className={`${s.input} ${s.marginAndPadding}`}
+          type="email"
+          name="userName"
+          id="userName"
+        />
+        <label className={s.marginAndPadding}>Create Password *</label>
+        <div className={`${s.passwordWithEye} ${s.marginAndPadding}`}>
+          <input
+            className={`${s.deleteBorder}`}
+            type="password"
+            name="password"
+            id="password"
+          />
+          <input type="button" value={this.state.eye} onChange={this.eye} />{" "}
+          {/* function needed for eye flip */}
+        </div>
+        <button className={`${s.input} ${s.marginAndPadding}`} type="submit">
+          SIGN IN
+        </button>
+      </div>
+    );
+  };
 
-    render() {
-        return(
-            <div className={`container`}>
-                <form onSubmit={this.submit}>
-                    <h3>New User or Returning?</h3>
-                    <input type="radio" name="userType" value="signIn" id="signIn" onChange={this.handleRadioButton}/> Sign In
-                    <input type="radio" name="userType" value="newUser" id="newUser" onChange={this.handleRadioButton}/> Create Account
-                    <label>Your E-Mail Address *</label>
-                    <input type="email" name="userName" id="userName" />
-                    <label>Create Password *</label>
-                    <div className={``}>
-                        <input className={``} type="password" name="password" id="password" />
-                        {/* {} */}
-                    </div>
-                    {this.loginType() ? this.newUser() : this.returningUser()}
-                </form>
+  loginType = () => (this.state.userType === "newUser" ? true : false);
+
+  render() {
+    return (
+      <div className={`container`}>
+        <form onSubmit={this.handleSubmit}>
+          <div className={s.formContainer}>
+            <button className={s.direction} onClick={this.backToStore}>
+              <i className={`fas fa-times`}></i>
+            </button>
+            <h3>New User or Returning?</h3>
+            <div>
+              <input
+                type="radio"
+                name="userType"
+                value="signIn"
+                id="signIn"
+                onChange={this.handleRadioButton}
+              />{" "}
+              Sign In
+              <input
+                type="radio"
+                name="userType"
+                value="newUser"
+                id="newUser"
+                onChange={this.handleRadioButton}
+              />{" "}
+              Create Account
             </div>
-        )
-    }
+          </div>
+          {this.loginType() ? this.newUser() : this.returningUser()}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
