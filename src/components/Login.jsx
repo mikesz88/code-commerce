a927be93da5e23477bdaecca9ff242e79cbbe860
michael <michaelsanchez8815@gmail.com>
2021-12-03 22:53:35 -0600

    email, password,name, postCode logic done on Login Comp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,34 @@
+/* 
+1. Create the Code for the error messages
+2. On submit I need to store the user on to the Commerce State User & change component to cart
+3. Create the logic for signing in 
+4. error message for email if already existed
+5. Make a firstName and LastName function to show diff error messages.
+6. Show a broad message on teh top above email address to show that there are error messages.
+6. CSS to show the UI from the mock up
+*/
+
 import React from "react";
 import s from "../components/Login.module.css";
+
+const INIT_CARD = {
+  password: '',
+  confirmPassword: '',
+  firstName: '',
+  lastName: '',
+  postCode: '',
+  email: ''
+}
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      eye: undefined,
+      eye: false,
+      revealPassword: 'password',
+      userType: 'newUser',
+      newUser: INIT_CARD,
+      error: {}
     };
   }
 
@@ -17,9 +40,127 @@ class Login extends React.Component {
     });
   };
 
+  checkErrorBeforeSave = () => {
+    const { newUser, error } = this.state;
+    let errorValue = {};
+    let isError = false;
+    Object.keys(newUser).forEach(val => {
+      let checkError = val;
+      if (!newUser[val].length || error[checkError]) {
+        error[checkError] 
+        ? errorValue = { ...errorValue, [checkError]: error[checkError]}
+        : errorValue = { ...errorValue, [checkError]: 'Required'};
+        isError = true;
+    }
+    });
+    this.setState({ error: errorValue });
+    return isError;
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
+    const errorCheck = this.checkErrorBeforeSave();
+    if (!errorCheck) {
+        this.setState({
+          newUser: INIT_CARD,
+        });
+    }  
   };
+
+  handleInputData = e => {
+    this.setState(prevState => ({
+      newUser: {
+        ...prevState.newUser,
+        [e.target.name]: e.target.value
+      }
+    }))
+  }
+
+  backToStore = () => {
+    this.setState({
+      newUser: INIT_CARD
+    })
+  }
+
+  eyeFlip = () => {
+    const eyeButton = document.getElementById('newUserPassword'); 
+    if (!this.state.eye) {
+        eyeButton.innerHTML = '<i class="fas fa-eye"></i>';
+        this.setState({
+          eye: true,
+          revealPassword: 'text'
+        });
+      } else {
+        eyeButton.innerHTML = '<i class="fas fa-eye-slash"></i>';
+        this.setState({
+          eye: false,
+          revealPassword: 'password'
+        });
+      }
+  }
+
+  lettersOnlyCheck = value => {
+    const letterRegex = /^[A-Za-z]+((\s)?((\'|\-|\.)?([A-Za-z])+))*$/gi;
+    const error = letterRegex.test(value);
+    return !error ? 'Must be letters Only' : undefined;
+  }
+
+  postCodeCheck = value => {
+    const postalCodeRegex = /^\d{5}$/;
+    const error = postalCodeRegex.test(value);
+    return !error ? 'Must be a 5 digit ZIP Code' : undefined;
+  }
+
+  passwordMatchCheck = value => 
+  !(value === this.state.newUser.password) 
+  ? 'This does not match your password above.' 
+  : undefined;
+
+  passwordValidationCheck = value => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,20}$/gm;
+    const error = passwordRegex.test(value);
+    return !error ? 'This does not fit the requirement. Try again!' : undefined;
+  }
+
+  emailCheck = value => {
+    const emailRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    const error = emailRegex.test(value);
+    return !error ? 'This is not a proper email. Try again!' : undefined;
+  }
+
+  handleValidations = (target, value) => {
+    let errorText;
+    switch (target) {
+      case 'email':
+        errorText = this.emailCheck(value);
+        this.setState(prevState => ({  error: {    ...prevState.error,    email: errorText }}))
+        break;
+      case 'password':
+        errorText = this.passwordValidationCheck(value);
+        this.setState(prevState => ({  error: {    ...prevState.error,    password: errorText }}))
+        break;
+      case 'confirmPassword':
+        errorText = this.passwordMatchCheck(value);
+        this.setState(prevState => ({  error: {    ...prevState.error,    confirmPassword: errorText }}))
+        break;    
+      case 'firstName':
+        errorText = this.lettersOnlyCheck(value);
+        this.setState(prevState => ({  error: {    ...prevState.error,    firstName: errorText }}))
+        break;      
+      case 'lastName':
+        errorText = this.lettersOnlyCheck(value);
+        this.setState(prevState => ({  error: {    ...prevState.error,    lastName: errorText }}))
+        break;
+      case 'postCode':
+        errorText = this.postCodeCheck(value);
+        this.setState(prevState => ({  error: {    ...prevState.error,    postCode: errorText }}))
+        break;
+      default:
+        break;
+    }
+  }
+
+  handleBlur = e => this.handleValidations(e.target.name, e.target.value);
 
   newUser = () => {
     return (
@@ -29,18 +170,26 @@ class Login extends React.Component {
         <input
           className={`${s.input} ${s.marginAndPadding}`}
           type="email"
-          name="userName"
-          id="userName"
+          name="email"
+          id="email"
+          autoComplete="off"
+          value={this.state.newUser && this.state.newUser.email}
+          onChange={this.handleInputData}
+          onBlur={this.handleBlur}
         />
         <label className={s.marginAndPadding}>Create Password *</label>
         <div className={`${s.passwordWithEye} ${s.marginAndPadding}`}>
           <input
+            id="revealPassword"
             className={`${s.deleteBorder}`}
-            type="password"
+            type={this.state.revealPassword}
             name="password"
-            id="password"
+            autoComplete="off"
+            value={this.state.newUser && this.state.newUser.password}
+            onChange={this.handleInputData}
+            onBlur={this.handleBlur}
           />
-          <input type="button" value={this.state.eye} onChange={this.eye} />{" "}
+          <button id="newUserPassword" type="button" onClick={this.eyeFlip}><i className="fas fa-eye-slash"></i></button>
           {/* function needed for eye flip */}
         </div>
         <p className={`${s.finePrint}`}>
@@ -54,6 +203,10 @@ class Login extends React.Component {
           type="password"
           name="confirmPassword"
           id="confirmPassword"
+          autoComplete="off"
+          value={this.state.newUser && this.state.newUser.confirmPassword}
+          onChange={this.handleInputData}
+          onBlur={this.handleBlur}
         />
         <label className={s.marginAndPadding}>First Name *</label>
         <input
@@ -61,6 +214,9 @@ class Login extends React.Component {
           type="text"
           name="firstName"
           id="firstName"
+          value={this.state.newUser && this.state.newUser.firstName}
+          onChange={this.handleInputData}
+          onBlur={this.handleBlur}
         />
         <label className={s.marginAndPadding}>Last Name *</label>
         <input
@@ -68,6 +224,10 @@ class Login extends React.Component {
           type="text"
           name="lastName"
           id="lastName"
+          autoComplete="off"
+          value={this.state.newUser && this.state.newUser.lastName}
+          onChange={this.handleInputData}
+          onBlur={this.handleBlur}
         />
         <label className={s.marginAndPadding}>PostCode</label>
         <input
@@ -75,6 +235,10 @@ class Login extends React.Component {
           type="number"
           name="postCode"
           id="postCode"
+          autoComplete="off"
+          value={this.state.newUser && this.state.newUser.postCode}
+          onChange={this.handleInputData}
+          onBlur={this.handleBlur}
         />
         <button className={`${s.input} ${s.marginAndPadding}`} type="submit">
           SAVE
@@ -119,7 +283,7 @@ class Login extends React.Component {
             name="password"
             id="password"
           />
-          <input type="button" value={this.state.eye} onChange={this.eye} />{" "}
+          <input type="button" value={this.state.eye} onClick={this.eyeFlip} />{" "}
           {/* function needed for eye flip */}
         </div>
         <button className={`${s.input} ${s.marginAndPadding}`} type="submit">
@@ -147,7 +311,7 @@ class Login extends React.Component {
                 value="signIn"
                 id="signIn"
                 onChange={this.handleRadioButton}
-              />{" "}
+              />
               Sign In
               <input
                 type="radio"
@@ -155,7 +319,7 @@ class Login extends React.Component {
                 value="newUser"
                 id="newUser"
                 onChange={this.handleRadioButton}
-              />{" "}
+              />
               Create Account
             </div>
           </div>
